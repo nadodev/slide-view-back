7cdf000354e0fb099f0a20181c85860fc57c1cac
Leonardo geja <bemmenos@gmail.com>
2025-11-29 16:08:25 -0300

    fix: Adicionar import do Log facade

--- app/Http/Controllers/Api/SocialAuthController.php
+++ app/Http/Controllers/Api/SocialAuthController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 
 class SocialAuthController extends Controller
